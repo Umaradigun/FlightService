de663346e4462708715f4ef29bcbd14b3eacb1cb
Umaradigun <adigunfaruk09061@gmail.com>
2024-08-07 03:19:55 -0400

    Working on the city model and also migrations

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const { PORT } = require('./config/serverConfig')
 
+
 const ServerSetup = async () =>{
     const app = express();
     app.use(bodyParser.json);
     app.use(bodyParser.urlencoded({ extended: true}));
     
+    
 
-    app.listen(PORT, () => {
+    
+    app.listen(PORT, async() => {
         console.log(`Server is running on http://localhost:${PORT}`)
+        
     })
 }
 
