5a6c1a97826d18001a3468e934748072f8782f73
Umaradigun <adigunfaruk09061@gmail.com>
2024-08-08 05:10:02 -0400

    complete adding routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 
 const v1ApiRoutes = require ('./v1');
+// const v2ApiRoutes = require ('./v2')
+// This should have be our version 2 api in case there is need
 
 router.use('v1/', v1ApiRoutes);
 
