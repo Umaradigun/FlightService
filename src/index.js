3f227257d175e5423d405df7f411c2cc41a72360
Umaradigun <adigunfaruk09061@gmail.com>
2024-08-08 04:41:33 -0400

    Imported api routes into src/index file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const { PORT } = require('./config/serverConfig')
-const CityRepository = require('./repository/city-repository');
-const AirportRepository = require('./repository/airport-repository');
+
+
+const ApiRoutes = require('./routes/index')
 
 const ServerSetup = async () =>{
     const app = express();
     app.use(bodyParser.json);
     app.use(bodyParser.urlencoded({ extended: true}));
     
-        const repo = new AirportRepository();
+        
 
-    // try {
-    //     repo.deleteAirport(9)
-    // } catch (error) {
-    //     throw error
-    // }
     
     app.listen(PORT, async() => {
         console.log(`Server is running on http://localhost:${PORT}`)
