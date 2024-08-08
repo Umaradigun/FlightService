a10ac46d0a6386c5ac62958453af633694f64aab
Umaradigun <adigunfaruk09061@gmail.com>
2024-08-08 02:15:05 -0400

    Created instance of city service and setup promises

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,47 @@
+const { cityService, cityService } = require('../services/index');
+
+const cityService = new cityService();
+
+
+const  create = async (req, res) => {
+    try {
+       
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const update = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+
+const destroy = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+
+const get = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+
+module.exports = {
+    create,
+    destroy,
+    update,
+    get
+}
